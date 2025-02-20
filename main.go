1043c2d46ff9026bf9caa89cc18ced9870b204c8
Adrian <adrian.hamzah15@gmail.com>
2025-02-20 12:02:47 +0700

    feat: add util to print slice

--- main.go
+++ main.go
@@ -1,1 +1,9 @@
- 
+package main
+
+import "github.com/mnqrt/go-print-slice/printslice"
+
+func main() {
+	a := make([]int, 3)
+	a[0], a[1], a[2] = 2, 4, 1
+	printslice.PrintSlice(a)
+}
--- printslice/printslice.go
+++ printslice/printslice.go
@@ -1,7 +1,20 @@
 package printslice
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func PrintFormat(s string) {
-	fmt.Println("---", s, "---")
+func PrintSlice(slice []int) {
+	for _, v := range slice {
+		fmt.Print(v,  " ")
+	}
+}
+
+func Print2DSlice(slice [][]int) {
+	for i := range slice {
+		for j := range slice[i] {
+			fmt.Print(slice[i][j], "\t")
+		}
+		fmt.Println()
+	}
 }
